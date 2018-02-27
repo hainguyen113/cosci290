5daedd6ce26e01f7ce6b4a1cc25ae45930bfcd27
henrynguyen85 <henrynguyen85@gmail.com>
2018-02-27 18:04:08 -0500

    inclass notes 2/27/18

---
+++ Lab5.java
@@ -0,0 +1,18 @@
+/*
+  Hai Nguyen
+  CoSci 290
+  
+  fix the logic and make this lab work
+  ask for user name
+  ask for more then 2 characters
+  say can not have numbers
+  
+*/
+
+public class Lab5
+{
+  public static void main(String[] args)
+  {
+  
+  }
+}
