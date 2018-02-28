bb0be88b03d7bb80606eeb6177d276a01fe3fc42
henrynguyen85 <henrynguyen85@gmail.com>
2018-02-27 20:00:49 -0500

    not done

--- Lab5.java
+++ Lab5.java
@@ -20,7 +20,7 @@ public class Lab5
     
     int nameLength = 0;
     String answer = "";
-    boolean flag = true; //flag is optional
+    boolean flag = false; //flag is optional
     
     //example of a while loop
     while(nameLength < 2)
@@ -30,48 +30,38 @@ public class Lab5
     
       //get answer from console
       answer = input.next();
-      while(flag)
-      { 
+      
+      //check if name is less than 2 characters
+      if(answer.length() < 2)
+      {
+      System.out.println("Please enter in a name that is at least "
+                        + "two letters long and no numbers");
+        
+      }
+      else
+      { //name is at least 2 characters
         
         //check if name contains letter
-      
+        
         //use a for-loop because we know how many letters it has
         for(int index = 0; index < answer.length(); index++)
         {
           
-          System.out.println(answer.charAt(index));
+          //System.out.println(answer.charAt(index));
           
           //check if the character is a letter or number
           if(!Character.isLetter(answer.charAt(index)))
           { //if it's not a letter
-            flag = true;
-            System.out.println("Please enter in a name that is at least "
-                  + "two letters long and no numbers.");
-            answer = input.next();
-            break; //gets out of current loop
-            
-          }
-          else
-          {
-            flag = false;
-          }
-         }
-       }//end of for-loop
-      //check if name is less than 2 letters
-      if(answer.length() < 2)
-      {
-      System.out.println("Please enter in a name that is at least "
-                        + "two letters");
-               
-        //nameLength = answer.length();
-      }
-      
-      nameLength = answer.length();
-      
-      if(answer.length() >= 2 && !flag){
-        break;
-      }
+                   
+          
+          }    //break; //gets out of current loop
         
-    } //end while loop
-  } //end of main method
-} //end of class
+        }//end of for-loop
+        //nameLength = answer.length();
+      
+      
+      
+     } //end while loop
+    } //end of main method
+  }//end of class
+}
