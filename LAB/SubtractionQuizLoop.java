ad8780d17cc8d8c5026c0f8bc87040226aab6687
henrynguyen85 <henrynguyen85@gmail.com>
2018-03-06 18:17:26 -0500

    lab 6

---
+++ LAB/SubtractionQuizLoop.java
@@ -0,0 +1,61 @@
+/*
+  Hai Nguyen
+  CoSci 290
+  Lab 6
+*/
+import java.util.Scanner;//utility that allows us to use scanner for user inputs
+public class SubtractionQuizLoop
+{
+  public static void main(String[] args)
+  {
+    final int NUMBER_OF_QUESTIONS = 10; //number of questions 
+    int correctCount = 0; //count the number of correct answers
+    int count = 0; //count the number of questions
+    long startTime = System.currentTimeMillis(); //beginning time when you started the test
+    String output = " "; //outputs string is initially empty
+    Scanner input = new Scanner(System.in);
+
+    while(count < NUMBER_OF_QUESTIONS)
+    {
+      //generate 2 random single digit integers
+      int number1 = (int)(Math.random() * 10);
+      int number2 = (int)(Math.random() * 10);
+
+      //if number1 < number2 swap number1 with number2
+      if(number1 < number2)
+      {
+        int temp = number1;
+        number1 = number2;
+        number2 = temp;
+      }//end of if loop for placing larger number with smaller number
+
+      //prompt the student to answer what is number1 - number2?
+      System.out.println("What is " + number1 + "-" + number2 + " ?");
+      System.out.print("> ");
+      
+      int answer = input.nextInt();//user input assinged to answer
+      
+      //grade the answer and display the result
+      if(number1 - number2 == answer)
+      {
+        System.out.println("You Are Correct!\n*********************************************\n");
+      }//end of if loop for display answer
+      else
+        System.out.println("Your answer is wrong.\n" + number1 + " - " + number2 + " should be " 
+                           + (number1 - number2) + "\n*********************************************\n");
+      //increase the questions count
+      count++;
+
+      output += "\n" + number1 + " - " + number2 + " = " + answer 
+             + ((number1 - number2 == answer) ? " correct" : " wrong");
+
+    }//end of while loop
+    
+    long endTime = System.currentTimeMillis();//end time for when you finished test
+    long testTime = endTime - startTime;//time it took to finish test
+    
+    //prints out your score
+    System.out.println("Correct count is " + correctCount + "\nTest Time is "
+                      + testTime / 1000 + " seconds\n" + output);
+  }//end of main method
+}//end of class
