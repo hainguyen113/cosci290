28e489e0117dd7402e0778f85f0dd0f677b23362
henrynguyen85 <henrynguyen85@gmail.com>
2018-03-22 18:18:30 -0400

    lab11

---
+++ INCLASS/Palindrome.java
@@ -0,0 +1,39 @@
+//example from book pg 187 5.14
+import java.util.Scanner;
+public class Palindrome 
+{
+  /** Main method */
+  public static void main(String[] args) 
+  {
+    char ans;
+    do
+    {
+      // Create a Scanner
+      Scanner input = new Scanner(System.in);
+      // Prompt the user to enter a string
+      System.out.print("Enter a string: ");
+      String s = input.nextLine();
+      // The index of the first character in the string
+      int low = 0;
+      // The index of the last character in the string
+      int high = s.length() - 1;
+      boolean isPalindrome = true;
+      while (low < high) {
+      if (s.charAt(low) != s.charAt(high)) 
+      {
+        isPalindrome = false;
+        break;
+      }
+      low++;
+      high--;
+      }
+      if (isPalindrome)
+      System.out.println(s + " is a palindrome");
+      else
+      System.out.println(s + " is not a palindrome\n\n");
+      System.out.print("Do you have another to test?\n> ");
+      ans = input.nextChar();      
+      
+    }while(ans == 'Y' || ans == 'y');
+  }
+}
