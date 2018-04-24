195adebefacd39510934eced87a87ea9d706c327
henrynguyen85 <henrynguyen85@gmail.com>
2018-04-24 18:47:12 -0400

    Lab15

--- INCLASS/GameRpg.java
+++ INCLASS/GameRpg.java
@@ -1,7 +1,12 @@
 //Hai Nguyen
 //CoSci 290
 //123
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
+
 public class GameRpg{
   public static void main(String[] args){
     
@@ -30,10 +35,21 @@ public class GameRpg{
                       + "        !:!   :!:       :!:          :!:       :!:  \n"
                       + "    :::: ::    ::: :::   ::           ::        ::  \n"
                       + "    :: : :     :: :: :    :            :         :  \n"
-                      + " \n\n\n);
+                      + " \n\n\n");
     
     System.out.println(name + "You and the remaining " + survivors + " survivors head in separate directions.");
-    
-                      
-  }
+      
+    String FILENAME = "MyFile.txt";
+    try (BufferedWriter bw = new BufferedWriter(new FileWriter(FILENAME))) {
+
+	  String content = "This is the content to write into file\n";
+
+	  bw.write(content);
+
+	  System.out.println("Done");
+
+    } catch (IOException e) {
+
+      e.printStackTrace();                  
+    }
 }
